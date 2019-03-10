a055002d3c88222b89ae0ca71324bb400d0969ee
sean <31554433+siriusfinex@users.noreply.github.com>
2019-03-10 12:55:36 +0800

    bug fix

--- port-forward-debian.sh
+++ port-forward-debian.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
-
-#! /bin/bash
 #enable net.ipv4.ip_forward
 ipv4_forward_str=$(sed -n '/^net.ipv4.ip_forward=1/'p /etc/sysctl.conf)
-if [ "$ipv4_forward_str" == "net.ipv4.ip_forward=1" ]; then
+if [ "`$ipv4_forward_str" == "net.ipv4.ip_forward=1`" ]; then
         echo "net.ipv4.ip_forward=1 configure is right"
 else
         echo -e "net.ipv4.ip_forward=1" >> /etc/sysctl.conf && sysctl -p
@@ -15,20 +13,21 @@ echo -n "local ip:" ; read local_ip && echo -n "remote ip:" ; read remote_ip
 echo "mode 0 is single port forward;mode 1 is multiple port forward."
 echo -n "mode:" ; read mode
 if [ "$mode" == '0' ]; then
-  echo -n "port:" ; read port
-  iptables -t nat -A PREROUTING -p tcp --dport $port -j DNAT --to-destination $remote_ip:$port
-  iptables -t nat -A PREROUTING -p udp --dport $port -j DNAT --to-destination $remoteip:$port
-  iptables -t nat -A POSTROUTING -p udp -d $remote_ip --dport $port -j SNAT --to-source $local_ip
-  iptables -t nat -A POSTROUTING -p tcp -d $remote_ip --dport $port -j SNAT --to-source $local_ip
+  echo -n "port:" ; read single_port
+  port1=$single_port
+  port2=$single_port
 elif [ "$mode" == '1' ]; then
   echo -n "start port:" ; read start_port && echo -n "end port:" ; read end_port
-  iptables -t nat -A PREROUTING -p tcp -m tcp --dport $start_port:$end_port -j DNAT --to-destination $remote_ip:$start_port-$end_port
-  iptables -t nat -A PREROUTING -p udp -m udp --dport $start_port:$end_port -j DNAT --to-destination $remote_ip:$start_port-$end_port
-  iptables -t nat -A POSTROUTING -d $remote_ip -p tcp -m tcp --dport $start_port:$end_port -j SNAT --to-source $local_ip
-  iptables -t nat -A POSTROUTING -d $remote_ip -p udp -m udp --dport $start_port:$end_port -j SNAT --to-source $local_ip
+  port1=$start_port:$end_port
+  port2=$start_port-$end_port
 else
   echo "Failed,please read the tips,the mode value only 0 or 1"
 fi
+
+iptables -t nat -A PREROUTING -p tcp -m tcp --dport $port1 -j DNAT --to-destination $remote_ip:$port2
+iptables -t nat -A PREROUTING -p udp -m udp --dport $port1 -j DNAT --to-destination $remote_ip:$port2
+iptables -t nat -A POSTROUTING -p tcp -d $remote_ip --dport $port1 -j SNAT --to-source $local_ip
+iptables -t nat -A POSTROUTING -p udp -d $remote_ip --dport $port1 -j SNAT --to-source $local_ip
 
 #save port forward rules to iptables
 iptables-save > /etc/iptables.up.rules
